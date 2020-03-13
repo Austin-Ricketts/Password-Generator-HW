26d0e68225f56626a62567710b69c27f6e19920e
Austin Ricketts <austinricketts4@gmail.com>
2020-03-13 16:41:00 -0400

    attempt at pushing from one array into another, not functioning properly

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "{", "}", "^", "_", "`", "~", "|", "\\", "\""];
-var entireSelection = [upperCase, lowerCase, numbers, specialChar];
-var randUpper = (Math.random(entireSelection[0]) * upperCase.length + 1).toString(36).toUpperCase();
-var randLower = (Math.random(entireSelection[0]) * lowerCase.length + 1).toString(36).toLowerCase();
-//var totalResponse = Math.random(upperCase, lowerCase, numbers, specialChar);
+let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "{", "}", "^", "_", "`", "~", "|", "\\", "\""];
+var userChoice = [];
+var response1;
+//var entireSelection = [upperCase, lowerCase, numbers, specialChar];
 
 function generatePassword() {
  var passLength = prompt("How many characters would you like in your Password? You may choose any cardinal number from 8 to 128, but no less and no more.");
       console.log(passLength);
       parseInt(passLength);
       console.log(parseInt(passLength));
+      var lengthChoice = parseInt(passLength);
+      
+        
  var response1 = confirm("Would you like to use Upper Case letters? Press 'Confirm' for yes or 'Cancel' for no.");
- console.log(response1);
+ console.log(response1);       
  var response2 = confirm("Would you like to use lower case letters? Press 'Confirm' for yes or 'Cancel' for no.");
  console.log(response2);
  var response3 = confirm("Would you like to use numbers? Press 'Confirm' for yes or 'Cancel' for no.");
@@ -23,12 +25,29 @@ function generatePassword() {
  var response4 = confirm("Would you like to use Special Characters? Press 'Confirm' for yes or 'Cancel' for no.");
  console.log(response4);
 
- if (response1 && response2 && response3 && response4) {
-    for (var i = 0; i < passLength.length; i++) {
-      return (Math.random(upperCase, lowerCase, numbers, specialChar) * passLength.length + 1).toString(36); 
-       }
- }   
- 
+    if (response1) push1 = function() {
+      userChoice.push.apply(upperCase);
+      console.log(userChoice); 
+    }
+
+    if (response2) push2 = function() {
+      userChoice.push.apply(lowerCase);
+      console.log(userChoice); 
+    }
+
+    if (response3) push3 = function() {
+      userChoice.push.apply(numbers);
+      console.log(userChoice); 
+    }
+
+    if (response4) push4 = function() {
+      userChoice.push.apply(specialChar);
+      console.log(userChoice); 
+    }  
+
+  for (var i = 0; i < userChoice; i++) {
+    return (Math.random(userChoice) * lengthChoice.length + 1).toString(36);
+}
 }
 
 
